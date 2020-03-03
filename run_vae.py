4d02a2460c356e3423e94e9974dd61d8cbb2c4b6
t-k- <clock126@126.com>
2020-03-03 22:31:46 +0800

    clean up.

--- run_vae.py
+++ run_vae.py
@@ -1,12 +1,59 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+
+import torchvision
 from torchvision import datasets
 from torchvision import transforms
-import torchvision
+
 import matplotlib.pyplot as plt
-from config import Config
-import model
+
+
+class Config(object):
+    train_path ='./' # there is a ./MNIST data folder
+    img_size = 784
+    h_dim = 400
+    z_dim = 20
+    download = True
+    batch_size = 100
+    shuffle = True
+    num_workers = 2
+    lr = 1e-3
+    use_gpu = True
+    epoch = 20
+
+
+class VAE(nn.Module):
+    def __init__(self, cfg):
+        super(VAE, self).__init__()
+        self.cfg = cfg
+        self.encoder = nn.Sequential(
+            nn.Linear(cfg.img_size, cfg.h_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(cfg.h_dim, cfg.z_dim * 2))
+            # no Sigmoid here, it should produce log_var
+            # whose range is [-infty, +infty]
+
+        self.decoder = nn.Sequential(
+            nn.Linear(cfg.z_dim, cfg.h_dim),
+            nn.ReLU(),
+            nn.Linear(cfg.h_dim, cfg.img_size),
+            nn.Sigmoid())
+
+    def reparameterize(self, mean, log_var):
+        samples = Variable(torch.randn(mean.size(0), mean.size(1)))
+        if self.cfg.use_gpu:
+            samples = samples.cuda()
+        z = mean + samples * torch.exp(log_var / 2)
+        return z
+
+    def forward(self, x):
+        h = self.encoder(x)
+        mean, log_var = torch.chunk(h, 2, dim=1)
+        z = self.reparameterize(mean, log_var)
+        out = self.decoder(z)
+        return out, mean, log_var
 
 
 def train(**kwargs):
@@ -23,9 +70,10 @@ def train(**kwargs):
                                               batch_size=cfg.batch_size,
                                               shuffle=cfg.shuffle,
                                               num_workers=cfg.num_workers)
-    vae = model.VAE(cfg)
+    vae = VAE(cfg)
     if cfg.use_gpu:
-        vae.cuda()
+        print('use GPU!')
+        vae = vae.cuda()
 
     optimizer = torch.optim.Adam(vae.parameters(), lr=cfg.lr)
     data_iter = iter(data_loader)
@@ -37,13 +85,14 @@ def train(**kwargs):
 
     plt.ion()
     for epoch in range(cfg.epoch):
+        print('epoch', epoch)
         for i, (images, _) in enumerate(data_loader):
 
             images = Variable(images.view(images.size(0), -1))
             if cfg.use_gpu:
                 images = images.cuda()
             out, mean, log_var = vae(images)
-            reconst_loss = F.binary_cross_entropy(out, images, size_average=False)
+            reconst_loss = F.binary_cross_entropy(out, images, reduction='sum')
             kl_divergence = torch.sum(0.5 * (mean ** 2 + torch.exp(log_var) - log_var - 1))
 
             total_loss = reconst_loss + kl_divergence
@@ -53,13 +102,15 @@ def train(**kwargs):
 
             if i % 100 == 0:
                 plt.cla()
+
                 plt.subplot(1, 2, 1)
                 plt.imshow(images.data[0].view(28, 28).cpu().numpy(), cmap="gray")
-
                 plt.subplot(1, 2, 2)
                 plt.imshow(out.data[0].view(28, 28).cpu().numpy(), cmap="gray")
+
                 plt.draw()
-                plt.pause(0.01)
+                plt.pause(0.1)
+
         reconst_images, _, _ = vae(fixed_x)
         reconst_images = reconst_images.view(reconst_images.size(0), 1, 28, 28)
         torchvision.utils.save_image(reconst_images.data.cpu(),
